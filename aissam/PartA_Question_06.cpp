037448ba696e54d0b35f4ba9097fe39efd101451
Mohammed Aissam Balchakker <mohammedaissambalchakker@gmail.com>
2025-05-28 10:14:45 +0530

    Add files via upload

---
+++ aissam/PartA_Question_06.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+inline int square(int x) {
+    return x * x;
+}
+
+int main6() {
+    int number;
+
+    cout << "Enter a number to find its square: ";
+    cin >> number;
+    int result = square(number);
+
+    cout<<"Square of "<< number << " is: "<<result<<endl;
+
+     cout<<"\nAissam14"<<endl;
+    return 0;
+}
+
+
+
+/*
+Enter a number to find its square: 4
+Square of 4 is: 16
+
+Aissam14
+*/
+
